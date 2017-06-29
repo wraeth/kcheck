9407670988de3a156a7c3bcf18ee2ee01f887fff
Sam Jorna (wraeth) <wraeth@wraeth.id.au>
2017-06-29 22:08:20 +1000

    checker.py: read required symbols from config

--- kcheck/checker.py
+++ kcheck/checker.py
@@ -4,21 +4,83 @@
 Check required kernel configuration items.
 """
 
-import argparse
+import configargparse
+import configparser
 import logging
 
 log = logging.getLogger('checker')
 
 
-def check_config(args: argparse.Namespace) -> int:
+def check_config(args: configargparse.Namespace) -> int:
     """
     Entry point for command line utility.
 
-    :param args: argparse.Namespace from command line utility
+    :param args: configargparse.Namespace from command line utility
     :return: return value for command line utility
     """
-    assert isinstance(args, argparse.Namespace)
-
+    assert isinstance(args, configargparse.Namespace)
     log.debug('Module loaded - beginning kernel configuration check')
+
+    symbols = load_required_symbols(args.config)
     # TODO: put stuff here
     return 0
+
+
+def load_required_symbols(config_file: str) -> dict:
+    """
+    Reads the specified configuration file to get required kernel symbol states.
+
+    :param config_file: path to configuration file
+    :return: dict(symbol: [values])
+    """
+    assert isinstance(config_file, str)
+
+    log.info('Reading required kernel symbols from config')
+    config = configparser.ConfigParser(allow_no_value=True)
+
+    symbols = {}
+
+    log.debug('Using config file %s' % config_file)
+
+    try:
+        config.read(config_file)
+    except configparser.DuplicateOptionError as err:
+        log.critical(err.message)
+        raise
+
+    if 'ternary' in config.sections():
+        log.info('Loading ternary symbols')
+        for key in config['ternary']:
+            assert isinstance(key, str)
+            sym_name = key.upper()
+
+            if not sym_name.startswith('CONFIG_'):
+                sym_name = 'CONFIG_'+sym_name
+
+            value = config['ternary'][key]
+
+            if value is None:
+                value = 'YM'
+
+            value = list(value.upper())
+
+            log.debug('Got symbol %s with allowed values %s' % (sym_name, str(value)))
+            symbols[sym_name] = value
+
+    if 'string' in config.sections():
+        log.info('Loading string symbols')
+        for key in config['string']:
+            assert isinstance(key, str)
+            sym_name = key.upper()
+
+            if not sym_name.startswith('CONFIG_'):
+                sym_name = 'CONFIG_'+sym_name
+
+            # to retain format, this is encased in list
+            value = [config['string'][key]]
+
+            log.debug('Got symbol {!s} with value {!r}'.format(sym_name, value))
+            symbols[sym_name] = value
+
+    log.info('Loaded %d kernel symbols to check' % len(symbols.keys()))
+    return symbols
