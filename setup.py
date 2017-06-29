282af259c93212567e8c928f2aa5a38c212672ed
Sam Jorna (wraeth) <wraeth@wraeth.id.au>
2017-06-30 00:34:33 +1000

    setup.py: finish writing

--- setup.py
+++ setup.py
@@ -1,9 +1,24 @@
 #!/usr/bin/env python3
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='kcheck',
-      version='0.0.0a',
-      description='Utility for generating and checking required kernel config symbols',
-      author='Sam Jorna',
-      author_email='wraeth@wraeth.id.au')
+    version='0.0.1a',
+    description='Utility for generating and checking required kernel config symbols',
+    author='Sam Jorna',
+    author_email='wraeth@wraeth.id.au',
+
+    entry_points={
+        'console_scripts': [
+            'kcheck = kcheck.command:main'
+        ]
+    },
+
+    platforms=['Linux'],
+    packages=find_packages(),
+    install_requires=['configargparse'],
+
+    package_data={
+        '': ['LICENSE']
+    }
+)
