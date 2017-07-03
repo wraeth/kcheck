98cff6f4ace13f36c5b2fddf7109c18cd4c3baa8
Sam Jorna (wraeth) <wraeth@wraeth.id.au>
2017-07-03 11:45:40 +1000

    Use less magic when handling version
    Due to how portage handles installing the package, the version was not
    correctly set resulting in --version outputting invalid information.
    This swaps how version is defined from being in setup.py and having
    __init__ read from there, to defining in __init__ and getting setup.py
    to read from that.
    
    Additionally add some minor metadata to setup.py.

--- kcheck/__init__.py
+++ kcheck/__init__.py
@@ -5,25 +5,13 @@
 from pkg_resources import get_distribution, DistributionNotFound
 import os.path
 
-try:
-    _dist = get_distribution('foobar')
-    # Normalize case for Windows systems
-    dist_loc = os.path.normcase(_dist.location)
-    here = os.path.normcase(__file__)
-    if not here.startswith(os.path.join(dist_loc, 'foobar')):
-        # not installed, but there is another version that *is*
-        raise DistributionNotFound
-except DistributionNotFound:
-    __version__ = 'Please install this project with setup.py'
-else:
-    __version__ = _dist.version
-
 __author__ = "Sam Jorna (wraeth)"
 __copyright__ = "Copyright 2017, Sam Jorna"
 __license__ = "MIT"
 __maintainer__ = "Sam Jorna (wraeth)"
 __email__ = "wraeth@wraeth.id.au"
 __status__ = "prototype"
+__version__ = "0.0.1"
 
 ALLOWED_PKGMGR = [
     'portage'
--- setup.py
+++ setup.py
@@ -2,11 +2,14 @@
 
 from setuptools import setup, find_packages
 
+import kcheck
+
 setup(name='kcheck',
-    version='0.0.1',
+    version=kcheck.__version__,
     description='Utility for generating and checking required kernel config symbols',
     author='Sam Jorna',
     author_email='wraeth@wraeth.id.au',
+    url='https://github.com/wraeth/kcheck',
     license='MIT',
 
     entry_points={
