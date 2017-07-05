abf5d55a0d748b458eb954bd21fa5f04268228bf
Sam Jorna (wraeth) <wraeth@wraeth.id.au>
2017-07-06 00:33:13 +1000

    helpers.py: add colour definitions

--- kcheck/helpers.py
+++ kcheck/helpers.py
@@ -5,6 +5,14 @@
 __verbose = False
 
 
+class Colour:
+    green = '\033[92m'
+    yellow = '\033[93m'
+    red = '\033[91m'
+    bold = '\033[1m'
+    end = '\033[0m'
+
+
 def verbose_print(msg: str = '') -> None:
     """
     Print message to stdout if __verbose is True.
